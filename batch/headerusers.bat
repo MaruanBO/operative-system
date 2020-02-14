b432950debf87069d15c45cde50d5785432965bd
MaruanBO <maruanbueno55@gmail.com>
2020-02-14 20:04:09 +0100

    v1

---
+++ batch/headerusers.bat
@@ -0,0 +1,9 @@
+@echo off
+set var=
+color 30
+cls
+echo ==== Users management ===============
+echo 1. Create users.
+echo 2. Delete users.
+echo 3. Exit the program.
+echo =====================================
